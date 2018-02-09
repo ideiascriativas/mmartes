a00dfb0cc1a94679c35d9d91effb1fda4ffb9dce
dijalmasilva <dijalmacz@gmail.com>
2018-02-09 11:32:59 -0300

    Add effect to title-highlight and fixing problem in font-size of the slide-content

--- src/app/mock/news.mock.ts
+++ src/app/mock/news.mock.ts
@@ -4,19 +4,19 @@ export const NEWS: New[] = [
   {
     id: 1,
     title: 'Aqui utilizamos máquinas com',
-    titleHighlight: 'mais alta tecnologia.',
+    titleHighlight: 'mais alta tecnologia',
     description: 'Venha conhecer nosso trabalho!'
   },
   {
     id: 2,
     title: 'Seu carro ou parede com com papéis',
-    titleHighlight: 'personalizados.',
+    titleHighlight: 'personalizados',
     description: 'Com a melhor qualidade e criatividade do mercado.'
   },
   {
     id: 3,
     title: '',
-    titleHighlight: 'Copos personalizados.',
+    titleHighlight: 'Copos personalizados',
     description: 'Crie o copo dos seus sonhos!'
   }
 ];
